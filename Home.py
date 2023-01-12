cff3eb15a2919f9b687ce73b5e2094ef02f603ed
kehindebabawale <109902571+kehindebabawale@users.noreply.github.com>
2023-01-12 20:37:58 +0100

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+base="dark"
+primaryColor="purple"
+st.title("K2K Automl")
+st.image("Intoduction.png")
+st.header("Introduction")
+st.info("Hey!... Welcome to K2K AutoML, where finding best model for data got easier.")
+st.subheader("How to use......	:speech_balloon:")
+st.write("Firstly, upload the data (CSV files only) through Uploading page, then navigate to the EDA to investigate the data and lastly, Run modelling and download the best model....easier right?:rocket:.")
+st.write("Limitation, the app might crash if the data is too heavy beacuse of the  Streamlit resource Limits ")
+st.write("Develop by Babawale Kehinde (Data scientist and Machine learning Engineer). [Github](https://github.com/kehindebabawale/machine_leraning_auto) [linkedin](https://www.linkedin.com/in/kehindebabawale)")
